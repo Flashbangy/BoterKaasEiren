dabd01826e59d0aa38e1e71ae3763f8b83e031b2
Flashbangy <44573779+Flashbangy@users.noreply.github.com>
2019-06-26 13:00:43 +0200

    final

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//packages
 package tictactoe;
 import java.util.Arrays;
 import java.util.InputMismatchException;
@@ -28,7 +29,8 @@ static Scanner in;
 		System.out.println("Boter-kaas-en-eieren");
 		System.out.println("--------------------------------");
 		printBoard();
-		System.out.println("X gaat eerst, typ een nummer om te starten");
+                System.out.println("--------------------------------");
+		System.out.println("X gaat eerst, typ een nummer 1-9 om te starten");
 		while (winner == null) {
 			int numInput;
 			try {
@@ -42,7 +44,7 @@ static Scanner in;
 				System.out.println("Typ 0 tot en met 9; typ weer de nummer in:");
 				continue;
 			}
-                        //dit zorgt wie er aan de beurt is
+                        //dit zorgt wie er aan de beurt is als je nummer in typt
 			if (board[numInput-1].equals(String.valueOf(numInput))) {
 				board[numInput-1] = turn;
 				if (turn.equals("X")) {
@@ -53,17 +55,19 @@ static Scanner in;
 				printBoard();
 				winner = checkWinner();
 			} else {
+                            //als je nummer geeft die al op de bord bezet is
 				System.out.println("Het van is al bezet, kies een andere vak");
 				continue;
 			}
 		}
+                //einde spel messages
 		if (winner.equalsIgnoreCase("draw")) {
 			System.out.println("Gelijkspel!");
 		} else {
 			System.out.println("Gefeliciteerd! " + winner + " heeft gewonnen!");
 		}
 	}
-        //dit is de functie die de winnende combinatie checkt 
+        //dit is de string die de winnende combinatie checkt met situaties die winnend zijn
 	static String checkWinner() {
 		for (int a = 0; a < 8; a++) {
 			String line = null;
@@ -99,7 +103,7 @@ static Scanner in;
 				return "O";
 			}
 		}
-                //gelijkspel code als het niet case 1 tot en met 7 is
+                //code als het niet case 1 tot en met 7 is is het draw
 		for (int a = 0; a < 9; a++) {
 			if (Arrays.asList(board).contains(String.valueOf(a+1))) {
 				break;
